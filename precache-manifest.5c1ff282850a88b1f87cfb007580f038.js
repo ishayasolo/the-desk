c8175d422498d50fb2ce4785dadd5eae4845f093
ishayasolo <ishaya808@gmail.com>
2022-06-24 14:06:15 +0100

    Updates

---
+++ precache-manifest.5c1ff282850a88b1f87cfb007580f038.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45fa4591fe90a088e874b66c3e959701",
+    "url": "/the-desk/index.html"
+  },
+  {
+    "revision": "41fe2b23e7e688aefe6e",
+    "url": "/the-desk/static/css/main.dc35a410.chunk.css"
+  },
+  {
+    "revision": "9e8eee430cd825574b6d",
+    "url": "/the-desk/static/js/2.3d77be7f.chunk.js"
+  },
+  {
+    "revision": "75e95a905c520d3430b67e4b1e7bffe6",
+    "url": "/the-desk/static/js/2.3d77be7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c9e4e6c1d5c942302b4",
+    "url": "/the-desk/static/js/3.b5237e2d.chunk.js"
+  },
+  {
+    "revision": "41fe2b23e7e688aefe6e",
+    "url": "/the-desk/static/js/main.e8f74a38.chunk.js"
+  },
+  {
+    "revision": "98c863c47e8aeab51095",
+    "url": "/the-desk/static/js/runtime-main.cbd67812.js"
+  },
+  {
+    "revision": "f5e1e33f315fb8a42c5ed9a71f318e2c",
+    "url": "/the-desk/static/media/01-desk.f5e1e33f.jpg"
+  },
+  {
+    "revision": "2cb531f278fe627408b5f449dd9992ab",
+    "url": "/the-desk/static/media/02-desk.2cb531f2.jpg"
+  },
+  {
+    "revision": "0339e9e2f1dba5cbb14522c12b479f67",
+    "url": "/the-desk/static/media/03-desk.0339e9e2.jpg"
+  },
+  {
+    "revision": "7d0e8cd694e89b07d4d9f83cf1a390bc",
+    "url": "/the-desk/static/media/04-desk.7d0e8cd6.jpg"
+  },
+  {
+    "revision": "8665487a6a964228488028a0a8e13493",
+    "url": "/the-desk/static/media/05-desk.8665487a.jpg"
+  },
+  {
+    "revision": "c58c7d67f73b3d82f0f42e3928c6e10e",
+    "url": "/the-desk/static/media/06-desk.c58c7d67.jpg"
+  },
+  {
+    "revision": "e05ddf4b35819ec0164b4406439d6176",
+    "url": "/the-desk/static/media/07-desk.e05ddf4b.jpg"
+  },
+  {
+    "revision": "463856b1453fe3d8d97261df10d0b446",
+    "url": "/the-desk/static/media/08-desk.463856b1.jpg"
+  },
+  {
+    "revision": "63c1e9c2174612ce7198e65f7905d73a",
+    "url": "/the-desk/static/media/access-icon.63c1e9c2.svg"
+  },
+  {
+    "revision": "e42340cfa0eeb15c2e482266fafd39f7",
+    "url": "/the-desk/static/media/accessible-icon.e42340cf.svg"
+  },
+  {
+    "revision": "a32d744693db00de7f9e83a5aac6003d",
+    "url": "/the-desk/static/media/bars-solid.a32d7446.svg"
+  },
+  {
+    "revision": "12fe3cb15c25e7c5128cd6bc2d2773c0",
+    "url": "/the-desk/static/media/board-room.12fe3cb1.jpg"
+  },
+  {
+    "revision": "bdd4b3e55447904a43c6504ed401626d",
+    "url": "/the-desk/static/media/cancel.bdd4b3e5.svg"
+  },
+  {
+    "revision": "e05ddf4b35819ec0164b4406439d6176",
+    "url": "/the-desk/static/media/chair-room.e05ddf4b.png"
+  },
+  {
+    "revision": "aaeb5386160b34148f5bcfda16f032f6",
+    "url": "/the-desk/static/media/coliseum.aaeb5386.jpg"
+  },
+  {
+    "revision": "862debc310de050fbd56731c1a0f2ed6",
+    "url": "/the-desk/static/media/facebook.862debc3.svg"
+  },
+  {
+    "revision": "10c663bbd0a01aa501aaf257c1d7db23",
+    "url": "/the-desk/static/media/footer-logo.10c663bb.svg"
+  },
+  {
+    "revision": "0140e4d0200d6fc8cbca725cb26d6cf5",
+    "url": "/the-desk/static/media/instagram.0140e4d0.svg"
+  },
+  {
+    "revision": "18bcdd8a7d7a8b494349a903f2e62277",
+    "url": "/the-desk/static/media/linkedin.18bcdd8a.svg"
+  },
+  {
+    "revision": "be2a31f53ba4e57cac1d9a2ba936723d",
+    "url": "/the-desk/static/media/popup-image-update.be2a31f5.png"
+  },
+  {
+    "revision": "18f569e6cae7bd8873bdf8cbc842d858",
+    "url": "/the-desk/static/media/private-icon.18f569e6.svg"
+  },
+  {
+    "revision": "bba45d041e35023d35ffcd342738935e",
+    "url": "/the-desk/static/media/startup-icon.bba45d04.svg"
+  },
+  {
+    "revision": "a81c41e87957fbfbc3c438dd2e9e9eca",
+    "url": "/the-desk/static/media/the-desk-bulb-logo.6a2228d2 1.a81c41e8.svg"
+  },
+  {
+    "revision": "59f812eaab4b4e039a4e5cae62603a3d",
+    "url": "/the-desk/static/media/the_desk.59f812ea.jpg"
+  },
+  {
+    "revision": "8a37d15dbf4161c81c7b16116bb1eb50",
+    "url": "/the-desk/static/media/top-floor.8a37d15d.jpg"
+  },
+  {
+    "revision": "03bb0fb74370146a90fba0ad0222ea8f",
+    "url": "/the-desk/static/media/twitter.03bb0fb7.svg"
+  },
+  {
+    "revision": "8b2bc5479088c47f1204372099aa7ed0",
+    "url": "/the-desk/static/media/youtube.8b2bc547.svg"
+  }
+]);
